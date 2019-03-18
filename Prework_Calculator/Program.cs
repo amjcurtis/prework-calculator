cc6b39766d5c7b63a8d8d874ed632a65f85c6ec2
amjcurtis <curtis.andrewm@gmail.com>
2019-03-18 12:12:50 -0700

    Add working methods for multiplication and division

--- Prework_Calculator/Program.cs
+++ Prework_Calculator/Program.cs
@@ -29,11 +29,29 @@ namespace Prework_Calculator
 
                 case "-":
                     Console.WriteLine("You've chosen subtraction.");
-                    Console.Write("Please enter your minuend. ");
+                    Console.Write("Please enter a minuend. ");
                     double minuend = double.Parse(Console.ReadLine());
-                    Console.Write("Please enter your subtrahend. ");
+                    Console.Write("Please enter a subtrahend. ");
                     double subtrahend = double.Parse(Console.ReadLine());
                     Subtract(minuend, subtrahend);
+                    break;
+
+                case "*":
+                    Console.WriteLine("You've chosen multiplication.");
+                    Console.Write("Please enter a multiplicand. ");
+                    double multiplicand = double.Parse(Console.ReadLine());
+                    Console.Write("Please enter a multiplier. ");
+                    double multiplier = double.Parse(Console.ReadLine());
+                    Multiply(multiplicand, multiplier);
+                    break;
+
+                case "/":
+                    Console.WriteLine("You've chosen division.");
+                    Console.Write("Please enter a dividend. ");
+                    double dividend = double.Parse(Console.ReadLine());
+                    Console.Write("Please enter a diminuend. ");
+                    double diminuend = double.Parse(Console.ReadLine());
+                    Divide(dividend, diminuend);
                     break;
 
                 default:
@@ -58,7 +76,17 @@ namespace Prework_Calculator
             Console.WriteLine($"The difference of your two numbers is {difference}.");
         }
 
+        private static void Multiply(double multiplicand, double multiplier)
+        {
+            double product = multiplicand * multiplier;
+            Console.WriteLine($"The product of your two numbers is {product}.");
+        }
 
+        private static void Divide(double dividend, double diminuend)
+        {
+            double quotient = dividend / diminuend;
+            Console.WriteLine($"The quotient of your two numbers is {quotient}.");
+        }
     }
 }
 
