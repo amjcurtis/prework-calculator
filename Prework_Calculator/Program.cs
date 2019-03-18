a3dcac9052635ddd3e801f7eb46f7287715154f7
amjcurtis <curtis.andrewm@gmail.com>
2019-03-18 12:02:07 -0700

    Add working methods for addition and subtraction

--- Prework_Calculator/Program.cs
+++ Prework_Calculator/Program.cs
@@ -6,7 +6,59 @@ namespace Prework_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's do some calculations!");
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("What kind of mathematical operation would you like to perform?");
+            Console.WriteLine("Your options are addition, subtraction, multiplication, and division.");
+            Console.WriteLine("Enter + to do addition, - to do subtraction, * to do multiplication, or / to do division.");
+
+            string userChoice = Console.ReadLine();
+
+            switch (userChoice)
+            {
+                case "+":
+                    Console.WriteLine("You've chosen addition.");
+                    Console.Write("Please enter your first addend. ");
+                    double addendOne = double.Parse(Console.ReadLine());
+                    Console.Write("Please enter your second addend. ");
+                    double addendTwo = double.Parse(Console.ReadLine());
+                    Add(addendOne, addendTwo);
+                    break;
+
+                case "-":
+                    Console.WriteLine("You've chosen subtraction.");
+                    Console.Write("Please enter your minuend. ");
+                    double minuend = double.Parse(Console.ReadLine());
+                    Console.Write("Please enter your subtrahend. ");
+                    double subtrahend = double.Parse(Console.ReadLine());
+                    Subtract(minuend, subtrahend);
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter just the operator of the operation you'd like to perform: +, -, *, or /");
+                    userChoice = Console.ReadLine(); // Need to rerun the method to give user another chance to do math operation
+                    break;
+            }
+
+            Console.WriteLine("Your math operation is complete. Press any key to end the program.");
+            Console.ReadLine();
         }
+
+        private static void Add(double addend1, double addend2) 
+        {
+            double sum = addend1 + addend2;
+            Console.WriteLine($"The sum of your two numbers is {sum}.");
+        }
+
+        private static void Subtract(double minuend, double subtrahend)
+        {
+            double difference = minuend - subtrahend;
+            Console.WriteLine($"The difference of your two numbers is {difference}.");
+        }
+
+
     }
 }
+
