b0dbcb54d82fa837181f4b4fa91eda16e14a68e7
amjcurtis <curtis.andrewm@gmail.com>
2019-03-18 13:23:19 -0700

    Refactor to let user do multiple successive math operations w/o exiting program

--- Prework_Calculator/Program.cs
+++ Prework_Calculator/Program.cs
@@ -7,61 +7,71 @@ namespace Prework_Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Let's do some calculations!");
-
             Console.WriteLine(" ");
 
             Console.WriteLine("What kind of mathematical operation would you like to perform?");
             Console.WriteLine("Your options are addition, subtraction, multiplication, and division.");
             Console.WriteLine("Enter + to do addition, - to do subtraction, * to do multiplication, or / to do division.");
+            string selectedOperation = Console.ReadLine();
 
-            string userChoice = Console.ReadLine();
-
-            switch (userChoice)
+            bool doMath = true;
+            while (doMath == true)
             {
-                case "+":
-                    Console.WriteLine("You've chosen addition.");
-                    Console.Write("Please enter your first addend. ");
-                    double addendOne = double.Parse(Console.ReadLine());
-                    Console.Write("Please enter your second addend. ");
-                    double addendTwo = double.Parse(Console.ReadLine());
-                    Add(addendOne, addendTwo);
-                    break;
+                switch (selectedOperation)
+                {
+                    case "+":
+                        Console.WriteLine("You've chosen addition.");
+                        Console.Write("Please enter your first addend. ");
+                        double addendOne = double.Parse(Console.ReadLine());
+                        Console.Write("Please enter your second addend. ");
+                        double addendTwo = double.Parse(Console.ReadLine());
+                        Add(addendOne, addendTwo);
+                        break;
 
-                case "-":
-                    Console.WriteLine("You've chosen subtraction.");
-                    Console.Write("Please enter a minuend. ");
-                    double minuend = double.Parse(Console.ReadLine());
-                    Console.Write("Please enter a subtrahend. ");
-                    double subtrahend = double.Parse(Console.ReadLine());
-                    Subtract(minuend, subtrahend);
-                    break;
+                    case "-":
+                        Console.WriteLine("You've chosen subtraction.");
+                        Console.Write("Please enter a minuend. ");
+                        double minuend = double.Parse(Console.ReadLine());
+                        Console.Write("Please enter a subtrahend. ");
+                        double subtrahend = double.Parse(Console.ReadLine());
+                        Subtract(minuend, subtrahend);
+                        break;
 
-                case "*":
-                    Console.WriteLine("You've chosen multiplication.");
-                    Console.Write("Please enter a multiplicand. ");
-                    double multiplicand = double.Parse(Console.ReadLine());
-                    Console.Write("Please enter a multiplier. ");
-                    double multiplier = double.Parse(Console.ReadLine());
-                    Multiply(multiplicand, multiplier);
-                    break;
+                    case "*":
+                        Console.WriteLine("You've chosen multiplication.");
+                        Console.Write("Please enter a multiplicand. ");
+                        double multiplicand = double.Parse(Console.ReadLine());
+                        Console.Write("Please enter a multiplier. ");
+                        double multiplier = double.Parse(Console.ReadLine());
+                        Multiply(multiplicand, multiplier);
+                        break;
 
-                case "/":
-                    Console.WriteLine("You've chosen division.");
-                    Console.Write("Please enter a dividend. ");
-                    double dividend = double.Parse(Console.ReadLine());
-                    Console.Write("Please enter a diminuend. ");
-                    double diminuend = double.Parse(Console.ReadLine());
-                    Divide(dividend, diminuend);
-                    break;
+                    case "/":
+                        Console.WriteLine("You've chosen division.");
+                        Console.Write("Please enter a dividend. ");
+                        double dividend = double.Parse(Console.ReadLine());
+                        Console.Write("Please enter a diminuend. ");
+                        double diminuend = double.Parse(Console.ReadLine());
+                        Divide(dividend, diminuend);
+                        break;
 
-                default:
-                    Console.WriteLine("Please enter just the operator of the operation you'd like to perform: +, -, *, or /");
-                    userChoice = Console.ReadLine(); // Need to rerun the method to give user another chance to do math operation
-                    break;
+                    default:
+                        Console.WriteLine("Please enter just the operator of the operation you'd like to perform: +, -, *, or /");
+                        Console.WriteLine(" ");
+                        // selectedOperation = Console.ReadLine(); 
+                        break;
+                }
+
+                Console.WriteLine("To restart and do another math operation, type the operator of the operation you'd like to do. Or hit Enter to exit the program. ");
+                selectedOperation = Console.ReadLine();
+                if (selectedOperation != "+" &&
+                    selectedOperation != "-" &&
+                    selectedOperation != "*" &&
+                    selectedOperation != "/")
+                {
+                    doMath = false;
+                }
             }
-
-            Console.WriteLine("Your math operation is complete. Press any key to end the program.");
-            Console.ReadLine();
         }
 
         private static void Add(double addend1, double addend2) 
